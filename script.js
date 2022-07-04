c11ac8b914ae285066a13e0663e543d1b8424eac
FranciscoVergaraC <franciscovergaracr@gmail.com>
2022-07-04 09:43:01 -0300

    region selector

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let newCountry = document.createElement("option");
 newCountry.value = "Bolivia";
 newCountry.id = "BO";
 newCountry.text = "Bolivia";
-console.log(newCountry)
 
+let selectContry = document.getElementById('selectContry');
 
 
 /*Aseguramos que se cargue window antes de empezar a crear y generar elementos*/
@@ -13,20 +13,34 @@ window.onload = () => {
     getCountries().then(data => {
         for (let i = 0; i < data.length; i++) {
             let newCountry = document.createElement("option");
-            newCountry.value = data[i].name;
+            newCountry.value = data[i].country;
             newCountry.id = data[i].code;
-            newCountry.text = data[i].name;
+            newCountry.text = data[i].country;
             countriesSelector.appendChild(newCountry);
         }
     }).catch(error => {
         console.log(error);
-    })
-}
+    });
+    selectContry.onclick = () => {
+        let countrySelector = document.getElementById('countries');
+        
+        getCities(countryCode).then(data => {
+            let citiesSelector = document.getElementById('cities');
+            citiesSelector.innerHTML = '';
+            for (let i = 0; i < data.length; i++) {
+                let newCity = document.createElement("option");
+                newCity.value = data[i].city;
+                newCity.id = data[i].code;
+                newCity.text = data[i].city;
+                citiesSelector.appendChild(newCity);
+            }
 
-/*
-newCountry.value = "Bolivia";
-countriesSelector.appendChild(newCountry)
-*/
+    });
+
+    getCities(`CN`).then(
+        console.log('request success')
+    );
+}};
 
 const getCountries = async () =>{
     try {
@@ -38,7 +52,6 @@ const getCountries = async () =>{
         });
         let data = await response.json();
         if (response.ok){
-            console.log(data[0].name)
             return data;
         }
     } catch (error){
@@ -46,6 +59,24 @@ const getCountries = async () =>{
         console.log(error);
     }
 }
+
+const getCities = async (countryCode) =>{
+    console.log('getcities called');
+    try {
+        let response = await fetch(`http://localhost:4001/${countryCode}/cities`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            }
+        });
+        let data = await response.json();
+        if (response.ok){
+            console.log(data)
+            return data;
+        }
+    } catch (error){
+    }
+    }
 
 let countries = getCountries();
 
