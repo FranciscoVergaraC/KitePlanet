6cdf1ef4d27fe6b5b7a7bb87bc848987fc76fc59
FranciscoVergaraC <franciscovergaracr@gmail.com>
2022-07-12 08:58:36 -0300

    SpotDisplay

--- script.js
+++ script.js
@@ -4,30 +4,54 @@
 window.onload = () => {
     let countriesSelector = document.getElementById('countries');
     let selectCountry = document.getElementById('selectCountry');
+    let findSpot = document.getElementById('findSpot');
+    let spotList = document.getElementById('spotList');
     getCountries().then(data => {
-        for (let i = 0; i < data.length; i++) {
-            let newCountry = document.createElement("option");
-            newCountry.value = data[i].code;
-            newCountry.text = data[i].country;
-            countriesSelector.appendChild(newCountry);
-        }
+        populateList(countriesSelector, "option", data, "code", "country")
     }).catch(error => {
         console.log(error);
     });
     selectCountry.onclick = () => {
-        
         let countriesSelector = document.getElementById('countries');
         let countryCode = countriesSelector.options[countriesSelector.selectedIndex].value;
         console.log(`Se ingresa a elegir regiones de pais con codigo ${countryCode}`);
         getRegions(countryCode).then(data => {
             let regionSelector = document.getElementById('region');
             removeAllChildNodes(regionSelector);
-            for (let i = 0; i < data.length; i++) {
-                let newRegion = document.createElement("option");
-                newRegion.value = data[i].shortCode;
-                newRegion.text = data[i].name;
-                regionSelector.appendChild(newRegion);
-            }}).catch(error => {console.log(error);});
+            populateList(regionSelector, "option", data, "shortCode", "name");
+
+        }).catch(error => {console.log(error);});
+            findSpot.style.display = "inline";
+            findSpot.onclick = () => {
+                get('spot').then(data => {    
+                removeAllChildNodes(spotList);
+                for(let i=0; i<data.rows.length; i++){
+                    let newSpot = document.createElement('tr');
+                    let newSpotName = document.createElement('td');
+                    newSpotName.innerHTML = data.rows[i].name;
+                    let spotCountry = document.createElement('td');
+                    spotCountry.innerHTML = data.rows[i].countryCode;
+                    let spotRegion = document.createElement('td');
+                    spotRegion.innerHTML = data.rows[i].regionCode;
+                    let windDirection = document.createElement('td');
+                    windDirection.innerHTML = data.rows[i].windDirection;
+                    newSpot.appendChild(newSpotName);
+                    newSpot.appendChild(spotCountry);
+                    newSpot.appendChild(spotRegion);
+                    newSpot.appendChild(windDirection);
+                    spotList.appendChild(newSpot);
+                }
+            });}
+    }
+};
+
+
+const populateList = (elementId, htmlType, data, value, text) => {
+    for(let i = 0; i < data.length; i++){
+        let newElement = document.createElement(htmlType);
+        newElement.value = data[i][value];
+        newElement.text = data[i][text];
+        elementId.appendChild(newElement);
     }
 };
 
@@ -62,6 +86,7 @@ const getRegions = async (countryCode) =>{
             return data;
         }
     } catch (error){
+        console.log(error);
     }
     }
 
@@ -71,3 +96,22 @@ const getRegions = async (countryCode) =>{
             parent.removeChild(parent.firstChild);
         }
     }
+
+    const get = async (param) =>{
+        try {
+            let response = await fetch(`http://localhost:4001/${param}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
+            });
+            let data = await response.json();
+            if (response.ok){
+                return data;
+            }
+        } catch (error){
+            // TODO: Manejor de errores
+            console.log(error);
+        }
+    }
+
